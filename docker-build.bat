12d7ac2c6de49f5a11aa288112d362074588e3c8
Elliot Alexander <epurvis97@gmail.com>
2017-09-30 23:21:21 +0100

    Added windows docker init script.

--- docker-build.bat
+++ docker-build.bat
@@ -1,3 +1,21 @@
 @echo off
 
+::IF %~dp0 NEQ %cd do cd %~dp0
+cd %~dp0
 
+net session >nul 2>&1
+ if %errorLevel% == 0 (
+          ECHO Beginning build.
+          docker build . -t "reddit-notifier:RNOTIFIER"
+          ECHO Docker build complete.
+          docker run -i -d -p %DOCKER_EXTERNAL_PORT%:80 -t reddit-notifier:RNOTIFIER
+          GOTO End
+ ) else (
+          ECHO Error! Run me with admin rights.
+          GOTO End
+ )
+
+
+:End
+ECHO End of script.
+pause >nul
